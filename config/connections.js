e2e72d508af0794d0dcee16f0729a7f840643c8a
Edgar Saavedra <edgarsaavedraa@gmail.com>
2017-08-24 10:22:04 -0700

    update db prefs

--- config/connections.js
+++ config/connections.js
@@ -43,9 +43,11 @@ module.exports.connections = {
  someMysqlServer: {
    adapter: 'sails-mysql',
    host: 'localhost',
+   port: 3306,
    user: 'root', //optional
    password: 'root', //optional
-   database: 'edgarsaavedra-sails-fauxinsta' //optional
+   database: 'edgarsaavedra-sails-fauxinsta', //optional
+   socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
  },
 
   /***************************************************************************
